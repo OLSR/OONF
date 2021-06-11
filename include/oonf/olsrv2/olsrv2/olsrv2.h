ba83590cb2785ab98daf62f0ca3ce1d1bd7d49f7
Henning Rogge <henning.rogge@fkie.fraunhofer.de>
2021-06-11 13:16:20 +0200

    ANSN Check must be skipped if we reset the sequence number of a router

--- include/oonf/olsrv2/olsrv2/olsrv2.h
+++ include/oonf/olsrv2/olsrv2/olsrv2.h
@@ -75,7 +75,7 @@ EXPORT uint64_t olsrv2_get_tc_interval(void);
 EXPORT uint64_t olsrv2_get_tc_validity(void);
 EXPORT bool olsrv2_is_nhdp_routable(struct netaddr *addr);
 EXPORT bool olsrv2_is_routable(struct netaddr *addr);
-EXPORT bool olsrv2_mpr_shall_process(struct rfc5444_reader_tlvblock_context *, uint64_t vtime);
+EXPORT bool olsrv2_mpr_shall_process(struct rfc5444_reader_tlvblock_context *, uint64_t vtime, bool *);
 EXPORT bool olsrv2_mpr_shall_forwarding(
   struct rfc5444_reader_tlvblock_context *context, struct netaddr *source_address, uint64_t vtime);
 EXPORT void olsrv2_generate_tcs(bool);
